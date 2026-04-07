3ceb3d04118469f52c873d16ebc9ddb9a0775b6b
Azizbek Asadov <azizbek.asadov.work@gmail.com>
2026-04-08 00:06:17 +0200

    Add Dummy Class in the component, and ignore validation of the swiftlint package in the packages on CI.

---
+++ Apps/Pomodoro/Pomodoro/components/DummyComponent.swift
@@ -0,0 +1,11 @@
+//
+//  DummyComponent.swift
+//  Pomodoro
+//
+//  Created by Azizbek Asadov on 08.04.2026.
+//  Copyright © 2026 Azizbek Asadov. All rights reserved.
+//
+
+import Foundation
+
+private final class DummyComponent {}
